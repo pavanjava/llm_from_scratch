2860755bc04eee90a7bcf450a0b8f36ce16954e4
Kameshwara Pavan Kumar Mantha <25398886+pavanjava@users.noreply.github.com>
2025-02-28 22:32:51 +0530

    Token embeddings (#7)
    * tokenization implementations
    
    * basic input output pairs
    
    * custom dataset creator
    
    * fully working dataset loader
    
    * added the token embedding prilim code
    
    * refactored entire workspace
    
    * refactored with tiktoken
    
    ---------
    
    Co-authored-by: pavanmantha <pavan.mantha@thevaslabs.io>

--- 3_token_embeddings/simple_embedding.py
+++ 3_token_embeddings/simple_embedding.py
@@ -1,15 +1,19 @@
 import torch
+import tiktoken
 
 sentence = "learning NLP and Large Language Models is very interesting"
+encoder = tiktoken.get_encoding("gpt2")
+# print(tiktoken.list_encoding_names())
+ids = encoder.encode(text=sentence, allowed_special="all")
 
-tokens = sorted(sentence.split(" "))
-vocab = [{token: idx} for idx, token in enumerate(tokens)]
+# tokens = sorted(sentence.split(" ")) # just for simplicity
+# vocab = [{token: idx} for idx, token in enumerate(tokens)]
 
-vocab_size = len(vocab)
+# vocab_size = len(vocab)
 dimension_size = 4
 
 torch.manual_seed(42)
-embedding_layer = torch.nn.Embedding(num_embeddings=vocab_size, embedding_dim=dimension_size)
+embedding_layer = torch.nn.Embedding(num_embeddings=len(ids), embedding_dim=dimension_size)
 
 print(embedding_layer.weight)
 
