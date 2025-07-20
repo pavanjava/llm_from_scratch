d93a98a0f5181dcb3e6462afe649638aa1b81205
pavanmantha <pavan.mantha@thevaslabs.io>
2025-07-20 15:01:18 +0530

    refactored the attention mechanism implementation

--- attention_mechanism_4/simple_attention.py
+++ attention_mechanism_4/simple_attention.py
@@ -1,26 +1,50 @@
 import torch
-import numpy as np
-import matplotlib.pyplot as plt
+import asyncio
 
 
-inputs = torch.tensor(
-    [
-        np.random.rand(1,3)[0], # your
-        np.random.rand(1,3)[0], # journey
-        np.random.rand(1,3)[0], # of
-        np.random.rand(1,3)[0], # NLP
-        np.random.rand(1,3)[0], # looks
-        np.random.rand(1,3)[0] # Awesome
-    ]
-)
+async def compute_attention_scores(inputs: torch.tensor, debug: bool = False):
+    attn_scores = torch.empty(inputs.shape[0])
+    query = inputs[1]
+    for i, x_i in enumerate(inputs):
+       attn_scores[i] = torch.dot(x_i, query) # dot product to compute the similarity of the words
+    if debug:
+        print(f"Raw Attention scores: {attn_scores}")
+    return attn_scores
 
-query = inputs[1]
+async def compute_attention_weights(attention_scores: torch.tensor, debug: bool = False):
+    # standard torch implementation of softmax
+    attn_weights = torch.softmax(attention_scores, dim=0)
+    if debug:
+        print(f"std.softmax normalised attention (attention weights): {attn_weights}")
+    return attn_weights
 
-print(inputs)
-print(query)
+# attn_scores_normalized = attention_scores / attention_scores.sum()
+# print(f"Normalised attention scores: {attn_scores_normalized}")
 
-attn_scores = torch.empty(inputs.shape[0])
-for i, x_i in enumerate(inputs):
-   attn_scores[i] = torch.dot(x_i, query)
+# naive implementation of softmax
+# def softmax_naive(x):
+#     return torch.exp(x) / torch.exp(x).sum(dim=0)
+#
+# attn_scores_normalized_2 = softmax_naive(attention_scores)
+# print(f"naive.softmax normalised attention (attention weights): {attn_scores_normalized_2}")
 
-print(attn_scores)
+
+async def main():
+    inputs = torch.tensor(
+        [
+            [0.8, 0.6, 0.1],        # I
+            [0.7, 0.7, 0.1],        # Love
+            [0.2, 0.8, 0.6],        # Coding
+            [0.1, 0.7, 0.7],        # GenAI
+        ]
+    )
+
+    attention_scores = await compute_attention_scores(inputs=inputs)
+    attention_weights = await compute_attention_weights(attention_scores=attention_scores)
+
+    print(f"attention scores: {attention_scores}")
+    print(f"attention weights: {attention_weights}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
